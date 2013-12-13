56a659a2783e998b416d7d6250a5ae4c52e4bcda
Irina Didkovskaya <irdidkovskaya@gmail.com>
2013-12-13 15:43:46 +0200

    Initial Commit

---
+++ Asteroids/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Asteroids
+//
+//  Created by Irina Didkovskaya on 12/13/13.
+//  Copyright (c) 2013 Irina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Asteroids/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Asteroids
+//
+//  Created by Irina Didkovskaya on 12/13/13.
+//  Copyright (c) 2013 Irina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
